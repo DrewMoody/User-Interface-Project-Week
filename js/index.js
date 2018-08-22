07cb8cdd2957f321204ed4efbce2e0b73fa47727
Drew Moody <drew.moody@me.com>
2018-08-22 13:10:44 -0400

    completed nav javascript

--- js/index.js
+++ js/index.js
@@ -1,1 +1,20 @@
 // JS goes here
+const mainHeader = document.querySelector('.main-header');
+const hamToggle = document.querySelectorAll('.main-header img');
+const hamClosed = document.querySelector('#hamclosed');
+const hamOpen = document.querySelector('#hamopen');
+const nav = document.querySelector('.show-nav');
+
+hamClosed.addEventListener('click', () => {
+  mainHeader.classList.toggle('sticky');
+  hamClosed.classList.toggle('hidden');
+  hamOpen.classList.toggle('hidden');
+  nav.classList.toggle('hidden');
+})
+
+hamOpen.addEventListener('click', () => {
+  mainHeader.classList.toggle('sticky');
+  hamClosed.classList.toggle('hidden');
+  hamOpen.classList.toggle('hidden');
+  nav.classList.toggle('hidden');
+})

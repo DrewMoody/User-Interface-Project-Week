fb74561248609744cd70e91379e5f2b8ff221dcf
Drew Moody <drew.moody@me.com>
2018-08-22 13:45:33 -0400

    cleanup up js

--- js/index.js
+++ js/index.js
@@ -5,16 +5,13 @@ const hamClosed = document.querySelector('#hamclosed');
 const hamOpen = document.querySelector('#hamopen');
 const nav = document.querySelector('.show-nav');
 
-hamClosed.addEventListener('click', () => {
+function navToggle() {
   mainHeader.classList.toggle('sticky');
   hamClosed.classList.toggle('hidden');
   hamOpen.classList.toggle('hidden');
   nav.classList.toggle('hidden');
-})
+};
 
-hamOpen.addEventListener('click', () => {
-  mainHeader.classList.toggle('sticky');
-  hamClosed.classList.toggle('hidden');
-  hamOpen.classList.toggle('hidden');
-  nav.classList.toggle('hidden');
-})
+hamClosed.addEventListener('click', navToggle);
+
+hamOpen.addEventListener('click', navToggle);

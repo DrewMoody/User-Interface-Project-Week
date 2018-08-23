f7692d671d642d484eeaa5a814979f5189c84b62
Drew Moody <drew.moody@me.com>
2018-08-22 20:41:55 -0400

    added services page

--- js/index.js
+++ js/index.js
@@ -1,4 +1,3 @@
-// JS goes here
 const mainHeader = document.querySelector('.main-header');
 const hamToggle = document.querySelectorAll('.main-header img');
 const hamClosed = document.querySelector('#hamclosed');
@@ -15,3 +14,42 @@ function navToggle() {
 hamClosed.addEventListener('click', navToggle);
 
 hamOpen.addEventListener('click', navToggle);
+
+// SERVICES TABS
+class TabLink {
+  constructor(element) {
+    this.element = element;
+    this.data = this.element.dataset.tab;
+    this.item = document.querySelector(`.tab-content[data-tab="${this.data}"]`);
+    this.tabItem = new TabItem(this.item);
+    this.element.addEventListener('click', () => {
+      this.select();
+    });
+  };
+  select() {
+    const tabs = document.querySelectorAll(`.tab`);
+    tabs.forEach(tab => {
+      tab.classList.remove('selected-tab');
+    });
+    this.element.classList.add('selected-tab');
+    this.tabItem.select();
+  };
+};
+
+class TabItem {
+  constructor(element) {
+    this.element = element;
+  }
+
+  select() {
+    console.log(this);
+    const items = document.querySelectorAll('.tab-content');
+    items.forEach(item => {
+      item.classList.remove('show-content');
+    });
+    this.element.classList.add('show-content');
+  };
+};
+
+let tabs = document.querySelectorAll('.tab');
+tabs = Array.from(tabs).map(tab => new TabLink(tab));
